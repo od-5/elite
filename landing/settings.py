d956c073f54e45666d32fff48e299f90f056aff2
alexy <od-5@yandex.ru>
2015-09-14 16:06:20 +0300

    a

--- landing/settings.py
+++ landing/settings.py
@@ -14,7 +14,7 @@ DEBUG = False
 
 TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = ['elitkadom.ru', 'арт-лифт.рф']
+ALLOWED_HOSTS = ['elitkadom.ru', u'арт-лифт.рф']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
